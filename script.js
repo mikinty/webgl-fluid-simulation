f32807c2a66548f0a6463cc0a5546ecb512c2062
Michael You <youmichaelc@gmail.com>
2023-01-07 16:47:25 -0500

    subtract 2 footer height

--- script.js
+++ script.js
@@ -1649,7 +1649,7 @@ footerElement.addEventListener("mousemove", (e) => {
     }
   }
   let posX = scaleByPixelRatio(e.offsetX);
-  let posY = scaleByPixelRatio(e.offsetY + canvas.height - footerElement.clientHeight);
+  let posY = scaleByPixelRatio(e.offsetY + canvas.height - 2*footerElement.clientHeight);
   updatePointerMoveData(pointer, posX, posY);
 });
 
